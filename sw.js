3d080b895b30cffe3c0d32d43490feec3f357b91
Madhuresh Sheth <63546523+madhuresh-sheth@users.noreply.github.com>
2020-04-12 19:31:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var cacheName = 'hello-pwa'; 
 var filesToCache = [
-'/',    
-'/index.html',    
-'/css/style.css',  
-'/js/main.js'  ];  
+'',    
+'index.html',    
+'css/style.css',  
+'js/main.js'  ];  
 self.addEventListener('install', function(e) { 
 e.waitUntil(
 caches.open(cacheName).then(function(cache) { 
